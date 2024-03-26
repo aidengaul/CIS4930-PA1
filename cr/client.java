190bc527281a3bbb592e00c7f103f597a4492325
Ashley M <ashleyemaurer@gmail.com>
2024-03-26 13:24:41 -0400

    old pa1 code

--- cr/client.java
+++ cr/client.java
@@ -3,47 +3,29 @@ import java.io.*;
 import java.net.*;
 
 public class client {
-    private DatagramSocket socket = null;
-    //private DataInputStream in = null;
-    private BufferedReader in = null;
-    //private DataOutputStream out = null;
+    private Socket socket = null;
+    private DataInputStream in = null;
+    private DataOutputStream out = null;
     private String inFromServer = "";
     private String outToServer = "";
     private BufferedReader reader = null;
 
-    public client(InetAddress IPAddress, int port) {
+    public client(String hostname, int port) {
         try {
             // Setting up connection
-            System.out.println("Attempting to connect to " + IPAddress + " on port " + port);
-            socket = new DatagramSocket(port);
-            System.out.println("Successfully connected to " + IPAddress + " on port " + port);
+            System.out.println("Attempting to connect to " + hostname + " on port " + port);
+            socket = new Socket(hostname, port);
+            System.out.println("Successfully connected to " + hostname + " on port " + port);
 
             // Initializing input/output streams
-            //in = new DataInputStream(socket.getInputStream());
-            //out = new DataOutputStream(socket.getOutputStream());
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
             reader = new BufferedReader(new InputStreamReader(System.in));
 
             //Server sends "Hello!" to ensure connection is established before client can send a message
-            //inFromServer = in.readUTF();
-            //System.out.println("Received from server: " + inFromServer);
+            inFromServer = in.readUTF();
+            System.out.println("Received from server: " + inFromServer);
 
-            byte[] sendData = new byte[1024];
-            byte[] receiveData = new byte[1024];
-
-            String sentence = reader.readLine();
-            sendData = sentence.getBytes();
-
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-            socket.send(sendPacket);
-
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            socket.receive(receivePacket);
-
-            String responseFromServer = new String(receivePacket.getData());
-
-            System.out.println("from server " + responseFromServer);
-            socket.close();
-            /* 
             while (!outToServer.equals("bye")) {
                 try {
                     System.out.print("Request server for: ");
@@ -74,7 +56,6 @@ public class client {
             out.close();
             in.close();
             socket.close();
-            */
         } catch (Exception e) {
             System.out.println(e);
             System.exit(-1);
@@ -84,10 +65,9 @@ public class client {
     public static void main(String[] args) {
         try {
             String hostname = args[0];
-            InetAddress IPAddress = InetAddress.getByName(hostname);
             int port = Integer.valueOf(args[1]);
             //Call client function with given port and hostname arguments to initialize client/client socket
-            client client = new client(IPAddress, port);
+            client client = new client(hostname, port);
         } catch (Exception e) {
             System.out.println(e);
             System.exit(-1);
